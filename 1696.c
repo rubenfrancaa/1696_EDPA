14597e245c0b96db548d03d8b4f0a19269f361e1
Ruben França <rubenaras@gmail.com>
2020-08-18 03:27:28 -0300

    Primeiros passos

---
+++ 1696.c
@@ -0,0 +1,124 @@
+//
+// Created by Rúben on 17/08/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct noh {
+    int valor;
+    int indice;
+    struct noh *esq, *dir;
+};
+typedef struct noh *TREE;
+
+void PesqIn(TREE arv);
+
+void PesqPRE(TREE arv);
+
+void insArvoreIN(TREE *arv, int vr, int ordemDeInsercao);
+
+TREE tPesq(TREE *arv, int indiceElemento);
+
+void alteraValor(TREE *arv, int indiceElemento, int novoValor);
+
+int main() {
+    // Instanciação das variáveis necessárias.
+    TREE arvore = NULL;
+    int casosDeTeste, quantidadeNumeros, quantidadeSubstituicoes, valorSeq, indice, valorB;
+
+    //Leitura da quantidade de casos de teste existentes.
+    scanf("%d", &casosDeTeste);
+
+    //Estrutura de repetição que será usado para controlar a quantidade de testes que o programa rodará.
+    while (casosDeTeste > 0) {
+
+        //Leitura do número de valores inteiros que farão parte da atual instância de teste.
+        scanf("%d", &quantidadeNumeros);
+
+        //Leitura da quantidade de substituições serão feitas.
+        scanf("%d", &quantidadeSubstituicoes);
+
+        //Laço para leitura e inserção dos valores na árvore.
+        for (int i = 1; i <= quantidadeNumeros; i++) {
+            scanf("%d", &valorSeq);
+            insArvoreIN(&arvore, valorSeq, i);
+        }
+        PesqPRE(arvore);
+        //Aqui deve ser realizado as contas para impressão de quem está ganhando até o momento.
+
+        for (int i = 0; i < quantidadeSubstituicoes; i++) {
+            //Leitura do valor que identificará qual elemento da sequência deve ser alterado.
+            // @indice: Dado encontrado dentro de cada nó que indica em qual ordem ele foi inserido. Ex: nó raíz tem indice = 1;
+            scanf("%d", &indice);
+            //leitura do valor entrará na sequência
+            scanf("%d", &valorB);
+
+            alteraValor(&arvore, 5 , valorB);
+            //A cada iteração deste laço deve ser realizado as novas contas para saber o resultado.
+        }
+
+
+        casosDeTeste -= 1;
+    }
+
+}
+
+void PesqIn(TREE arv) {
+//Infixa
+
+    if (arv != NULL) {
+        PesqIn(arv->esq);
+        printf("%i ", arv->valor);
+        printf("indice: %d \n", arv->indice);
+        PesqIn(arv->dir);
+    }
+}
+
+void PesqPRE(TREE arv) {
+//Pre Fixa
+
+    if (arv != NULL) {
+        printf("%i ", arv->valor);
+        printf("indice: %d \n", arv->indice);
+        PesqIn(arv->esq);
+        PesqIn(arv->dir);
+    }
+}
+
+void insArvoreIN(TREE *arv, int vr, int ordemDeInsercao) {
+    if (*arv == NULL) {
+        *arv = (TREE) malloc(sizeof(struct noh));
+        (*arv)->valor = vr;
+        (*arv)->indice = ordemDeInsercao;
+        (*arv)->esq = NULL;
+        (*arv)->dir = NULL;
+    } else {
+        if (vr < (*arv)->valor)
+            insArvoreIN(&((*arv)->esq), vr, ordemDeInsercao);
+        else
+            insArvoreIN(&((*arv)->dir), vr, ordemDeInsercao);
+    }
+}
+
+void alteraValor(TREE *arv, int indiceElemento, int novoValor) {
+    TREE nohAux = NULL;
+    nohAux = tPesq(&(*arv), indiceElemento);
+    if(nohAux) {
+        printf("Valor: %d | indice: %d\n ", nohAux->valor, nohAux->indice);
+        nohAux->valor = novoValor;
+    }
+}
+
+TREE tPesq(TREE *arv, int indiceElemento) {
+    if (!*arv)   //Elemento não encontrado
+        return (NULL);
+    else
+    if (indiceElemento == (*arv)->indice) // Elemento encontrado na raiz
+        return (*arv);
+    else
+    if (indiceElemento < (*arv)->valor)
+        return (tPesq(&((*arv)->esq),indiceElemento));
+    else
+        return (tPesq(&((*arv)->dir), indiceElemento));
+}
